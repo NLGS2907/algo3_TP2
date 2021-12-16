5e521ea5f03a9322832d237abbfe2c873e694940
ezezbogar <ezezbogar@gmail.com>
2021-12-16 17:12:12 -0300

    Mapa Singleton

--- src/main/java/edu/fiuba/algo3/modelo/Mapa.java
+++ src/main/java/edu/fiuba/algo3/modelo/Mapa.java
@@ -1,47 +1,46 @@
 package edu.fiuba.algo3.modelo;
 
-import org.json.simple.JSONObject;
-import org.json.simple.JSONArray;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
 import java.util.ArrayList;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Map;
 
 public class Mapa {
 
-    private Ciudad ciudadActual;
+    private static Mapa instancia = null;
+
     private ArrayList<Ciudad> ciudades;
     private Map<String, Map<String, Integer>> distancias;
 
-    Mapa(Ciudad ciudad) { // Cambiar tests
-        this.ciudadActual = ciudad; // Cambiar
-
+    private Mapa() {
         MapReader mapReader = new MapReader("src/main/java/edu/fiuba/algo3/config/ciudades.json");
         this.distancias = mapReader.cargarDistancias();
         this.ciudades = mapReader.cargarCiudades();
     }
 
-    public void realizarViaje(Ciudad destino, Detective detective) {
-        int distancia = this.calcularDistanciaEntre(ciudadActual, destino);
+    private static void crearMapa() {
+        if (instancia == null) {
+            instancia = new Mapa();
+        }
+    }
+
+    public static Mapa obtenerInstancia() {
+        if (instancia == null) {
+            crearMapa();
+        }
+        return instancia;
+    }
+
+    public void realizarViaje(Ciudad origen, Ciudad destino, Detective detective) {
+        int distancia = this.calcularDistanciaEntre(origen, destino);
         detective.viajar(distancia);
-        this.ciudadActual = destino;
     }
 
-    public void cargarMapa() {}
-    private int calcularDistancia(Ciudad ciudad){
-        return 3725;
-    }
+    //void crearRutaDelLadron() {
+    //    // Implementar
+    //}
 
-    public String ciudadActual() {
-        return this.ciudadActual.obtenerNombre();
-    }
-
-    void crearRutaDelLadron() {
-        // Implementar
-    }
+    //Ciudad ciudadInicial() {
+    //    return ciudades.get(0); // Cambiar
+    //}
 
     int calcularDistanciaEntre(Ciudad ciudadOrigen, Ciudad ciudadDestino) {
         return distancias.get(ciudadOrigen.obtenerNombre()).get(ciudadDestino.obtenerNombre());
@@ -49,5 +48,16 @@ public class Mapa {
 
     int calcularDistanciaEntre(String ciudadOrigen, String ciudadDestino) {
         return distancias.get(ciudadOrigen).get(ciudadDestino);
+    }
+
+    ////// TEST //////
+    public Ciudad obtenerCiudad(String nombre) {
+        Ciudad ciudadBuscada = null;
+        for (Ciudad ciudad : ciudades) {
+            if (ciudad.obtenerNombre().equals(nombre)) {
+                ciudadBuscada = ciudad;
+            }
+        }
+        return ciudadBuscada;
     }
 }
