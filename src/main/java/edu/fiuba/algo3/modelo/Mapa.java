3b707c26573fd3a29c1c77190e0ba86267dba74d
ezezbogar <ezezbogar@gmail.com>
2021-12-16 12:31:45 -0300

    Mapa practicamente terminado

--- src/main/java/edu/fiuba/algo3/modelo/Mapa.java
+++ src/main/java/edu/fiuba/algo3/modelo/Mapa.java
@@ -8,19 +8,20 @@ import org.json.simple.parser.ParseException;
 import java.util.ArrayList;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Map;
 
 public class Mapa {
 
     private Ciudad ciudadActual;
     private ArrayList<Ciudad> ciudades;
-    int[][] adyacencias = {{0, 1, 1, 0, 0},
-                            {1, 0, 0, 1, 1},
-                            {1, 0, 0, 0, 1},
-                            {0, 1, 0, 0, 1},
-                            {0, 1, 1, 1, 0}}; // Hardcodeado , leer del Json.
+    private Map<String, Map<String, Integer>> distancias;
+
     Mapa(Ciudad ciudad) { // Cambiar tests
-        this.ciudadActual = ciudad;
-        this.ciudades = new ArrayList<>();
+        this.ciudadActual = ciudad; // Cambiar
+
+        MapReader mapReader = new MapReader("src/main/java/edu/fiuba/algo3/config/ciudades.json");
+        this.distancias = mapReader.cargarDistancias();
+        this.ciudades = mapReader.cargarCiudades();
     }
 
     public void realizarViaje(Ciudad destino, Detective detective) {
@@ -29,6 +30,7 @@ public class Mapa {
         this.ciudadActual = destino;
     }
 
+    public void cargarMapa() {}
     private int calcularDistancia(Ciudad ciudad){
         return 3725;
     }
@@ -37,50 +39,15 @@ public class Mapa {
         return this.ciudadActual.obtenerNombre();
     }
 
-    public void cargarMapa() {
-        try {
-            FileReader reader = new FileReader("src/main/java/edu/fiuba/algo3/modelo/config/ciudades.json");
-            JSONParser mapParser = new JSONParser();
-
-            JSONObject jsonObject = (JSONObject) mapParser.parse(reader);
-            JSONArray ciudadesArray = (JSONArray) jsonObject.get("ciudades");
-
-            ciudadesArray.forEach(item -> {
-                //System.out.println(item.toString());
-                ciudades.add(new Ciudad(item.toString()));
-            });
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        // Cargar adyacencias.
-    }
-
     void crearRutaDelLadron() {
         // Implementar
     }
 
     int calcularDistanciaEntre(Ciudad ciudadOrigen, Ciudad ciudadDestino) {
-        Integer distancia = 0;
+        return distancias.get(ciudadOrigen.obtenerNombre()).get(ciudadDestino.obtenerNombre());
+    }
 
-        try {
-            FileReader reader = new FileReader("src/main/java/edu/fiuba/algo3/modelo/config/ciudades.json");
-            JSONParser mapParser = new JSONParser();
-            JSONObject jsonObject = (JSONObject) mapParser.parse(reader);
-
-            JSONObject ciudadesJSON = (JSONObject) jsonObject.get("distanciasCiudades");
-            JSONObject ciudadOrigenJSON= (JSONObject) ciudadesJSON.get(ciudadOrigen.obtenerNombre());
-            distancia = Integer.parseInt(ciudadOrigenJSON.get(ciudadDestino.obtenerNombre()).toString());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        return distancia;
+    int calcularDistanciaEntre(String ciudadOrigen, String ciudadDestino) {
+        return distancias.get(ciudadOrigen).get(ciudadDestino);
     }
 }
