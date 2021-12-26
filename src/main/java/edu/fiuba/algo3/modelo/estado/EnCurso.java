7c2bbb4679e47b2f49e0150cf0ec038cedb7ab87
ezezbogar <ezezbogar@gmail.com>
2021-12-26 17:03:18 -0300

    Agrego pistas

--- src/main/java/edu/fiuba/algo3/modelo/estado/EnCurso.java
+++
@@ -1,4 +0,0 @@
-package edu.fiuba.algo3.modelo.estado;
-
-public class EnCurso extends EstadoMision{
-}
--- src/main/java/edu/fiuba/algo3/modelo/estado/LadronEscapo.java
+++
@@ -1,4 +0,0 @@
-package edu.fiuba.algo3.modelo.estado;
-
-public class LadronEscapo extends EstadoMision{
-}
