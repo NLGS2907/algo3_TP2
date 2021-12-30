7730cf855fb6cd97e0a0487873dcba0cf7af65d5
Andy <amoyano@fi.uba.ar>
2021-12-30 16:16:53 -0300

    Mejora de la intuitividad de la interfaz

--- src/main/java/edu/fiuba/algo3/controlador/botones/contextuales/HandlerComenzarMision.java
+++ src/main/java/edu/fiuba/algo3/controlador/botones/contextuales/HandlerComenzarMision.java
@@ -5,6 +5,9 @@ import edu.fiuba.algo3.modelo.pistas.DescripcionesDeCiudades;
 import edu.fiuba.algo3.vista.animaciones.AnimacionTexto;
 import edu.fiuba.algo3.vista.cajas.CajaLugarYFecha;
 import edu.fiuba.algo3.vista.contenedores.CuadroDialogo;
+import edu.fiuba.algo3.vista.contenedores.MenuComenzarJuego;
+import edu.fiuba.algo3.vista.contenedores.MenuComenzarNuevaMision;
+import edu.fiuba.algo3.vista.contenedores.MenuInfoMision;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
@@ -12,8 +15,7 @@ public class HandlerComenzarMision implements EventHandler<ActionEvent> {
     @Override
     public void handle(ActionEvent actionEvent) {
         Juego.obtenerInstancia().empezarNuevaMision();
-        String descripcion = DescripcionesDeCiudades.obtenerInstancia().leerDescripcion(Juego.obtenerInstancia().ciudadActual().obtenerNombre());
-        CajaLugarYFecha.obtenerInstancia().mostrarLugarYTiempo();
-        CuadroDialogo.obtenerInstancia().actualizarTodo(new AnimacionTexto(descripcion).getEtiqueta());
+        CajaLugarYFecha.obtenerInstancia().mostrarCuartelGeneral();
+        CuadroDialogo.obtenerInstancia().actualizarTodo(new MenuInfoMision());
     }
 }
---
+++ src/main/java/edu/fiuba/algo3/vista/contenedores/MenuInfoMision.java
@@ -0,0 +1,20 @@
+package edu.fiuba.algo3.vista.contenedores;
+
+import edu.fiuba.algo3.modelo.Juego;
+import edu.fiuba.algo3.modelo.pistas.ContenedorDeTesoros;
+import edu.fiuba.algo3.vista.animaciones.AnimacionTexto;
+import edu.fiuba.algo3.vista.botones.contextuales.BotonComenzarMision;
+import edu.fiuba.algo3.vista.botones.contextuales.BotonContinuar;
+import edu.fiuba.algo3.vista.etiquetas.Etiqueta;
+import javafx.scene.layout.VBox;
+
+public class MenuInfoMision extends VBox {
+    public MenuInfoMision(){
+        super(5);
+        String ciudad = Juego.obtenerInstancia().ciudadActual().obtenerNombre();
+        String tesoro = ContenedorDeTesoros.obtenerInstancia().leerTesoro(ciudad);
+        String informeMision = "Un sospechoso no identificado robó " + tesoro + " de la ciudad " + ciudad + ".\nInvestigue el caso!";
+        this.getChildren().add(new AnimacionTexto(informeMision).getEtiqueta());
+        this.getChildren().add(new BotonContinuar());
+    }
+}
