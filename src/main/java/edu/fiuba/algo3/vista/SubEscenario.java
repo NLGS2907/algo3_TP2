0633ed55c51338515c39ff3a71a0000801d1b4f5
Franco Lighterman Reismann <flighterman@fi.uba.ar>
2021-12-30 17:08:32 -0300

    Agregado BotonEmitirOrdenDeArresto

--- src/main/java/edu/fiuba/algo3/vista/SubEscenario.java
+++ src/main/java/edu/fiuba/algo3/vista/SubEscenario.java
@@ -1,6 +1,7 @@
 package edu.fiuba.algo3.vista;
 
 import edu.fiuba.algo3.vista.imagenes.IconoAlgoThief;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
@@ -15,6 +16,7 @@ public class SubEscenario extends Stage {
     public SubEscenario(String titulo, Node... elementos) {
         super();
         StackPane layout = new StackPane(elementos);
+        layout.setAlignment(Pos.TOP_CENTER);
         Scene escena = new Scene(layout);
         this.setTitle(titulo);
         this.setScene(escena);
